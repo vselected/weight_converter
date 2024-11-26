584704dbf2a8cc737b58d78da4bde14f27281dc3
vselected <dominik.sarleja@gmail.com>
2024-11-26 14:57:43 +0100

    Create function that convert units

--- main.py
+++ main.py
@@ -9,8 +9,20 @@ def weight_converter():
         print("Pounds: 2")
 
         unit_convert_from = int(input("Please provide unit you want to convert from: "))
-        unit_convert_to = int(input("Please provide unit you want to convert to: "))
-        weight = float(input("Please provide weight: "))
+
+        # Function that convert
+        if unit_convert_from == 1:
+            unit_convert_to = int(input("Please provide unit you want to convert to: "))
+            if unit_convert_to == 1:
+                weight = float(input("Please provide weight: "))
+                print(f"{weight} Kilograms")
+        elif unit_convert_from == 2:
+            unit_convert_to = int(input("Please provide unit you want to convert to: "))
+            if unit_convert_to == 2:
+                weight = float(input("Please provide weight: "))
+                print(f"{weight} Pounds")
+        else:
+            print("Please provide valid number")
 
         # Create function that ask if you wanna still use converter
         next_convert = input("Do you want to do next convert? (y / n): ").lower()
