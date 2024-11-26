48802120b2066679ce48f412dd44043d50852a04
vselected <dominik.sarleja@gmail.com>
2024-11-26 14:51:44 +0100

    Create navigation

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 def weight_converter():
+    print("\n")
+    print("Welcome to Weight Converter!")
 
     while True:
+
+        print("Please pick number for specific Unit:")
+        print("Kilograms: 1")
+        print("Pounds: 2")
+
+        unit_convert_from = int(input("Please provide unit you want to convert from: "))
+        unit_convert_to = int(input("Please provide unit you want to convert to: "))
+        weight = float(input("Please provide weight: "))
 
         # Create function that ask if you wanna still use converter
         next_convert = input("Do you want to do next convert? (y / n): ").lower()
